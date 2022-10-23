65cd8653e1cfb264b5a23acf17f0251c9fb8e113
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-23 22:21:03 +0300

    feat: create auth-context, implement setting of new users to firebase

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { Routes, Route } from 'react-router-dom';
 import Account from './components/Account';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold">Firebase Auth Test App</h1>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    try {
+      await createUser(email, password);
+      navigate('/account');
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="max-w-[700px] mx-auto my-16 p-4">
       <div>
@@ -13,14 +32,22 @@ const Signup = () => {
           </Link>{' '}
         </p>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Email address</label>
-          <input className="border border-gray-300 p-3" type="email" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="email"
+          />
         </div>
         <div className="flex flex-col py-2">
           <label className="py-2 font-medium">Password</label>
-          <input className="border border-gray-300 p-3" type="password" />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="password"
+          />
         </div>
         <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full py-4 my-2 text-white">
           Create an account

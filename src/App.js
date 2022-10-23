c2aac6dff37950ef01f58ed249d8880f89507384
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-23 15:12:58 +0300

    feat: basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Account from './components/Account';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 function App() {
-  return <div className="bg-green-400">Test</div>;
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold">Firebase Auth Test App</h1>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

421c8fde6696257785e8c1a56f92b3eb168ddc15
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-23 16:00:19 +0300

    style: add tailwind classes to components

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div className="max-w-[600px] mx-auto my-16 p-4 ">
+      <h1 className="text-2xl font-bold py-4 text-center">
+        Hello, emailAddress! Welcome to your profile
+      </h1>
+      <div className="bg-blue-500 p-4 h-64 text-white flex flex-col justify-between">
+        <ul className="h-[50%] flex flex-col justify-between">
+          <li className="cursor-pointer">Photos</li>
+          <li className="cursor-pointer">Videos</li>
+          <li className="cursor-pointer">Music</li>
+        </ul>
+        <h3 className="self-end cursor-pointer">Other private stuff</h3>
+      </div>
+      <button className="border px-6 py-2 my-4">Log out</button>
+    </div>
+  );
 };
 
 export default Account;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="max-w-[700px] mx-auto my-16 p-4">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Register new account</h1>
+        <p className="py-2">
+          Already have an account yet?{' '}
+          <Link to="/" className="underline">
+            Sign in.
+          </Link>{' '}
+        </p>
+      </div>
+      <form>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Email address</label>
+          <input className="border border-gray-300 p-3" type="email" />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Password</label>
+          <input className="border border-gray-300 p-3" type="password" />
+        </div>
+        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full py-4 my-2 text-white">
+          Create an account
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;

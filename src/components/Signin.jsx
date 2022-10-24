5b26cd0a30a49cfee6775bb298996bdcfb33d761
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-24 22:33:35 +0300

    feat: add error handling

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -21,7 +21,7 @@ const Signin = () => {
     }
   };
 
-  return (
+  return !error ? (
     <div className="max-w-[700px] mx-auto my-16 p-4">
       <div>
         <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
@@ -54,7 +54,40 @@ const Signin = () => {
         </button>
       </form>
     </div>
+  ) : (
+    <div className="max-w-[700px] mx-auto my-16 p-4">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
+        <p className="py-2">
+          Dont't have an account yet?{' '}
+          <Link to="/signup" className="underline">
+            Sign up.
+          </Link>
+        </p>
+        <span className="text-red-400">Wrong login or password, try again</span>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Email address</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="email"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="password"
+          />
+        </div>
+        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full py-4 my-2 text-white">
+          Sign in
+        </button>
+      </form>
+    </div>
   );
 };
-
 export default Signin;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     }
   };
 
-  return (
+  return !error ? (
     <div className="max-w-[700px] mx-auto my-16 p-4">
       <div>
         <h1 className="text-2xl font-bold py-2">Register new account</h1>
@@ -31,6 +31,40 @@ const Signup = () => {
             Sign in.
           </Link>{' '}
         </p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Email address</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="email"
+          />
+        </div>
+        <div className="flex flex-col py-2">
+          <label className="py-2 font-medium">Password</label>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-gray-300 p-3"
+            type="password"
+          />
+        </div>
+        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full py-4 my-2 text-white">
+          Create an account
+        </button>
+      </form>
+    </div>
+  ) : (
+    <div className="max-w-[700px] mx-auto my-16 p-4">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Register new account</h1>
+        <p className="py-2">
+          Already have an account yet?{' '}
+          <Link to="/" className="underline">
+            Sign in.
+          </Link>{' '}
+        </p>
+        <span className="text-red-400">Email is already in use</span>
       </div>
       <form onSubmit={handleSubmit}>
         <div className="flex flex-col py-2">

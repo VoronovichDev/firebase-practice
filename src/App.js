4cf69a6271658716ffacad64d0646beab8d02885
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-24 22:26:14 +0300

    feat: redirect to Login component if there is no auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Account from './components/Account';
+import ProtectedRoute from './components/ProtectedRoute';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import { AuthContextProvider } from './context/AuthContext';
@@ -13,7 +14,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AuthContextProvider>
     </div>

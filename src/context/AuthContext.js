c80f185e040ef91ec9d3f2e9043484bc714813f7
Voronovich_dev <alexanderbizv1@gmail.com>
2022-10-24 22:20:54 +0300

    feat: implement sign-in logic

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,6 +16,10 @@ export const AuthContextProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const logout = () => {
     return signOut(auth);
   };
@@ -31,7 +35,9 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ createUser, user, logout }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ createUser, user, logout, signIn }}>
+      {children}
+    </UserContext.Provider>
   );
 };
 
